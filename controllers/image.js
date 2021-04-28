aec208c62d36292996d730d45cde83c184fedbb5
Amr Nashaat <amrnashaat8@gmail.com>
2021-04-28 23:05:43 +0200

    created delete image route

--- controllers/image.js
+++ controllers/image.js
@@ -1,4 +1,5 @@
 const Image = require("../models/Image");
+const fs = require("fs");
 const getAllImages = async (req, res, next) => {
   try {
     const limit = req.query.limit;
@@ -20,5 +21,20 @@ const getUserImage = async (req, res, next) => {
     next(error);
   }
 };
+const removeUserImage = async (req, res, next) => {
+  try {
+    const image = await Image.findById(req.params.id);
+    if (image.user.toString() !== req.user._id.toString()) {
+      throw new Error("this is not your image");
+    }
+    // remove from server
+    fs.unlinkSync(image.imagePath);
+    // remove from database
+    await Image.findOneAndRemove({ _id: req.params.id });
+    res.send({ message: "image removed" });
+  } catch (error) {
+    next(error);
+  }
+};
 
-module.exports = { getAllImages, getUserImage };
+module.exports = { getAllImages, getUserImage, removeUserImage };
--- index.js
+++ index.js
@@ -10,10 +10,7 @@ require("./config/db");
 app.use(morgan("tiny"));
 app.use(express.json());
 app.use(cors());
-app.use(
-  "/uploads",
-  express.static(path.join(__dirname, "uploads/userUploads"))
-);
+app.use("/uploads", express.static(path.join(__dirname, "uploads/")));
 app.use("/api/user", userRoutes);
 app.use("/api/image", imageRoutes);
 app.use((err, req, res, next) => {
